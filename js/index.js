6a6145f1971ab8c89774909eb8074e3845ce3895
Adam Purdy <purdyrad@Adams-MacBook-Air.local>
2021-02-08 18:24:03 -0500

    nearly complete

--- js/index.js
+++ js/index.js
@@ -40,3 +40,61 @@ const siteContent = {
 // Example: Update the img src for the logo
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
+
+// header
+
+const multipleSections = document.querySelectorAll('a');
+multipleSections[0].textContent = 'Services';
+multipleSections[1].textContent = 'Product';
+multipleSections[2].textContent = 'Vision';
+multipleSections[3].textContent = 'Features';
+multipleSections[4].textContent = 'About';
+multipleSections[5].textContent = 'Contact';
+
+const img = document.querySelector('#logo-img');
+img.src = 'img/logo.png';
+
+const cta = document.querySelector('.cta-text h1');
+cta.textContent = 'Dom Is Awesome';
+const ctaButton = document.querySelector('.cta-text button');
+ctaButton.textContent= 'Get Started';
+const ctaImg = document.querySelector('#cta-img');
+ctaImg.src ='img/header-img.png';
+
+// main content
+
+const topHeads = document.querySelectorAll('.top-content h4');
+topHeads[0].textContent = 'Features';
+topHeads[1].textContent = 'About';
+const topPs = document.querySelectorAll('.top-content p');
+topPs[0].textContent = 'Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+topPs[1].textContent = 'About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+// middle image
+
+const midImg = document.querySelector('#middle-img');
+midImg.src = 'img/mid-page-accent.jpg';
+
+// bottom content
+const botHeads = document.querySelectorAll('.bottom-content h4');
+botHeads[0].textContent = 'Services';
+botHeads[1].textContent = 'Product';
+botHeads[2].textContent = 'Vision';
+const botPs = document.querySelectorAll('.bottom-content p');
+botPs[0].textContent = 'Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.'
+botPs[1].textContent = 'Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+botPs[2].textContent = 'Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.';
+
+
+// Contact
+
+const contactHead = document.querySelector('.contact h4');
+contactHead.textContent = 'Contact';
+const contacts = document.querySelectorAll('.contact p');
+contacts[0].textContent = '123 Way 456 Street <br> Somewhere, USA';
+contacts[1].textContent = '1 (888) 888-8888';
+contacts[2].textContent = 'sales@greatidea.io';
+
+// Footer
+
+const foot = document.querySelector('footer p');
+foot.textContent = 'Copyright Great Idea! 2018';
